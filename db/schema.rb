7c76d48df07a6cc51f5e5e5301e0fe86097f6f34
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-22 18:07:19 -0300

    Add user id to resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_204109) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_210122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_204109) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
