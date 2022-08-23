4cdab621fb6750df3fd2bd97986bdb4be454b6cb
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-23 11:04:57 -0300

    Minor fix to curriculum affiliation migration
    By mistake I added curriculum_affiliation_id's to both curriculum and learning_unit models. This commit fixes that.

--- db/schema.rb
+++ db/schema.rb
@@ -33,14 +33,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_124350) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "curriculum_affiliation_id"
   end
 
   create_table "learning_units", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "curriculum_affiliation_id"
   end
 
   create_table "resource_comments", force: :cascade do |t|
