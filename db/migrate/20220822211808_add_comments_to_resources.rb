2d1aaa30874aa586f1700507c61bfe86b20043b7
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-25 17:27:57 -0300

    Create foreign keys instead of just references

--- db/migrate/20220822211808_add_comments_to_resources.rb
+++ db/migrate/20220822211808_add_comments_to_resources.rb
@@ -1,5 +1,5 @@
 class AddCommentsToResources < ActiveRecord::Migration[7.0]
   def change
-    add_column :resource_comments, :resource_id, :integer
+    add_reference :resource_comments, :resource, foreign_key: true
   end
 end
--- db/migrate/20220822212717_add_user_to_resource_evaluations.rb
+++ db/migrate/20220822212717_add_user_to_resource_evaluations.rb
@@ -1,5 +1,5 @@
 class AddUserToResourceEvaluations < ActiveRecord::Migration[7.0]
   def change
-    add_column :resource_evaluations, :user_id, :integer
+    add_reference :resource_evaluations, :user, foreign_key: true
   end
 end
