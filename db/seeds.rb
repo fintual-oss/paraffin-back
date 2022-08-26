d5a9fa4823648221bc16b74682816d142029dc40
alejandroleiva <aleivailabaca@gmail.com>
2022-08-26 09:21:35 -0300

    Format change in db/seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database 
+# with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created 
+# alongside the database with db:setup).
 
 f_user = User.create(name: 'Ale')
 s_user = User.create(name: 'Cris')
@@ -8,13 +10,14 @@ fisrt_curr = Curriculum.create(name: 'Fullstack Developer')
 f_learning_unit = LearningUnit.create(name: 'Ruby')
 s_learning_unit = LearningUnit.create(name: 'Rails')
 CurriculumAffiliation.create(curriculum: fisrt_curr,
-                                learning_unit: f_learning_unit)
-CurriculumAffiliation.create(curriculum: fisrt_curr, 
-                                learning_unit: s_learning_unit)
+                             learning_unit: f_learning_unit)
+CurriculumAffiliation.create(curriculum: fisrt_curr,
+                             learning_unit: s_learning_unit)
 f_resource = Resource.create(user: f_user, learning_unit: f_learning_unit,
-                                name: 'Ruby for dummies', url: 'fakeurl.io')
+                             name: 'Ruby for dummies', url: 'fakeurl.io')
 ResourceComment.create(user: s_user, resource: f_resource,
-                        content: 'Vale pico tu wea')
+                       content: 'Vale pico tu wea')
 ResourceEvaluation.create(user: t_user, resource: f_resource, evaluation: 5)
 ResourceEvaluation.create(user: s_user, resource: f_resource, evaluation: 3)
 CompletedLearningUnit.create(user: f_user, learning_unit: f_learning_unit)
+
