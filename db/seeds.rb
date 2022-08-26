be212246935d86267cfb8631c156642d5f600587
AlehandroL <100784837+AlehandroL@users.noreply.github.com>
2022-08-26 08:51:25 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-# This file should contain all the record creation needed to seed the database 
+# This file should contain all the record creation needed to seed the database
 # with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created 
+# The data can then be loaded with the bin/rails db:seed command (or created
 # alongside the database with db:setup).
 
 f_user = User.create(name: 'Ale')
@@ -20,4 +20,3 @@ ResourceComment.create(user: s_user, resource: f_resource,
 ResourceEvaluation.create(user: t_user, resource: f_resource, evaluation: 5)
 ResourceEvaluation.create(user: s_user, resource: f_resource, evaluation: 3)
 CompletedLearningUnit.create(user: f_user, learning_unit: f_learning_unit)
-
