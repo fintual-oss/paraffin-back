c0e36f49ce4bd37ddbf25ad3010f7b46c86ab586
ristofer <cristopher.gomez@ug.uchile.cl>
2022-08-29 17:17:46 -0300

    style: rewriting learning_unit model test

--- spec/models/learning_unit_spec.rb
+++ spec/models/learning_unit_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe LearningUnit, type: :model do
   it 'is not valid without a name' do
-    learning_unit = LearningUnit.new(name: nil)
-    expect(learning_unit).to_not be_valid
+    invalid_learning_unit = described_class.new(name: nil)
+    expect(invalid_learning_unit).to_not be_valid
   end
   it 'is valid with a name' do
-    learning_unit = LearningUnit.new(name: 'Ruby on rails')
-    expect(learning_unit).to be_valid
+    example_learning_unit = described_class.new(name: 'Ruby on rails')
+    expect(example_learning_unit).to be_valid
   end
 end
