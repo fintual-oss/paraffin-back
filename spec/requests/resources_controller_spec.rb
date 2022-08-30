589a3576cedfa9dfe5a69a935aadc246cb5d7e05
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-30 10:16:17 -0300

    test: resources controller

---
+++ spec/requests/resources_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+RSpec.describe ResourcesController, type: :request do
+  describe 'GET /show' do
+    let(:resource) { create(:resource) }
+    let(:comment) { create(:resource_comment, resource:) }
+    let(:comments) { [comment] }
+    let(:average) { 3 }
+    let(:resource_service_mock) do
+      instance_double(
+        Resources::ResourceService,
+        id: 4,
+        learning_unit_name: 'Ruby on Rails',
+        created_by: 'Matías Hurtado',
+        comments:,
+        average:
+      )
+    end
+
+    def perform
+      get resource_path(resource)
+    end
+    before do
+      allow(Resources::ResourceService)
+        .to receive(:new)
+        .with(resource)
+        .and_return(resource_service_mock)
+    end
+
+    it do
+      perform
+      expect(response.body).to include('Showing resource 4')
+    end
+
+    context 'when there are no comments' do
+      let(:comments) { [] }
+
+      it do
+        perform
+        expect(response.body).to include('There are no comments yet')
+      end
+    end
+
+    context 'when there are no evaluations' do
+      let(:average) { nil }
+
+      it do
+        perform
+        expect(response.body).to include('There are no evaluations yet')
+      end
+    end
+  end
+end
