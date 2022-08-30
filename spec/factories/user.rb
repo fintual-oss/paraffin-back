806272b3bda841ee8912a0d478f489286c6006be
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-30 11:15:07 -0300

    fix: user factory to comply with new model

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { 'John Doe' }
+    email { 'john@example.com' }
+    password { 'password' }
+    reset_password_token { '1234567890' }
   end
 end
