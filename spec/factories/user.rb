0c8e4e8de485ec627ab04e3f6d72d279fc90f65c
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-30 11:29:28 -0300

    fix: user factory - sequenced email

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
     name { 'John Doe' }
-    email { 'john@example.com' }
+    sequence :email do |n|
+      "john_#{n}@example.com"
+    end
     password { 'password' }
     reset_password_token { '1234567890' }
   end
