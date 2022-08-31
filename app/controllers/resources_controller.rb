449f05185fefcc0c5db2a69a6d740dd34ca387bf
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-31 18:06:15 -0300

    feat: evaluation from resource controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,5 +2,7 @@ class ResourcesController < ApplicationController
   def show
     resource = Resource.find(params[:id])
     @service = Resources::ResourceService.new(resource)
+    @evaluation = ResourceEvaluation
+      .find_by(user: current_user, resource:)
   end
 end
