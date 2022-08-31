b73cb411ee1925691d1804afd42b14a57e3b397e
Alejandro Leiva <aleivailabaca@gmail.com>
2022-08-31 14:57:31 -0300

    test: implement tests for factories in curriculum and curriculum_affiliation

--- spec/models/curriculum_affiliation_spec.rb
+++ spec/models/curriculum_affiliation_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe CurriculumAffiliation, type: :model do
+  context 'when creating an object with the factory' do
+    it do
+      expect(build(:curriculum_affiliation)).to be_valid
+    end
+  end
+
   it 'is valid with valid attributes'
   it 'is not valid without a curriculum_id'
   it 'is not valid without a learning_unit_id'
