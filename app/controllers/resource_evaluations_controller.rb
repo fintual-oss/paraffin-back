ed964ea5885ce133e9b67692ff604531340f51e9
Ronald Tichauer <rtichauerv@gmail.com>
2022-08-31 18:21:47 -0300

    linting correctly

--- app/controllers/resource_evaluations_controller.rb
+++ app/controllers/resource_evaluations_controller.rb
@@ -3,7 +3,8 @@ class ResourceEvaluationsController < ApplicationController
     evaluation = params['resource_evaluations']['evaluation']
     resource_id = params['resource_id']
     resource_evaluation = ResourceEvaluation
-      .find_or_initialize_by(user_id: current_user.id, resource_id:)
+                          .find_or_initialize_by(user_id: current_user.id,
+                                                 resource_id:)
     resource_evaluation.evaluation = evaluation
     resource_evaluation.save if resource_evaluation.has_changes_to_save?
     redirect_to(Resource.find(resource_id))
--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -3,6 +3,6 @@ class ResourcesController < ApplicationController
     resource = Resource.find(params[:id])
     @service = Resources::ResourceService.new(resource)
     @evaluation = ResourceEvaluation
-      .find_by(user: current_user, resource:)
+                  .find_by(user: current_user, resource:)
   end
 end
