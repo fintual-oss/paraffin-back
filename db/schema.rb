a54151d52da30d9edef2b37e4578bc229d068aa3
Alejandro Leiva <aleivailabaca@gmail.com>
2022-08-31 16:08:19 -0300

    implement tests for completed_learning_unit model and factory

--- db/schema.rb
+++ db/schema.rb
@@ -43,19 +43,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_141732) do
   create_table 'completed_learning_units', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'learning_unit_id'
-    t.index ['learning_unit_id'], name: 'index_completed_learning_units_on_learning_unit_id'
-    t.index ['user_id'], name: 'index_completed_learning_units_on_user_id'
+    t.integer 'user_id', null: false
+    t.integer 'learning_unit_id', null: false
   end
 
   create_table 'curriculum_affiliations', force: :cascade do |t|
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'curriculum_id'
-    t.bigint 'learning_unit_id'
-    t.index ['curriculum_id'], name: 'index_curriculum_affiliations_on_curriculum_id'
-    t.index ['learning_unit_id'], name: 'index_curriculum_affiliations_on_learning_unit_id'
+    t.integer 'curriculum_id'
+    t.integer 'learning_unit_id'
   end
 
   create_table 'curriculums', force: :cascade do |t|
@@ -74,20 +70,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_141732) do
     t.text 'content'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'resource_id'
-    t.index ['resource_id'], name: 'index_resource_comments_on_resource_id'
-    t.index ['user_id'], name: 'index_resource_comments_on_user_id'
+    t.integer 'user_id'
+    t.integer 'resource_id'
   end
 
   create_table 'resource_evaluations', force: :cascade do |t|
     t.integer 'evaluation'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'resource_id'
-    t.bigint 'user_id'
-    t.index ['resource_id'], name: 'index_resource_evaluations_on_resource_id'
-    t.index ['user_id'], name: 'index_resource_evaluations_on_user_id'
+    t.integer 'resource_id'
+    t.integer 'user_id'
   end
 
   create_table 'resources', force: :cascade do |t|
@@ -95,10 +87,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_141732) do
     t.string 'url'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'learning_unit_id'
-    t.index ['learning_unit_id'], name: 'index_resources_on_learning_unit_id'
-    t.index ['user_id'], name: 'index_resources_on_user_id'
+    t.integer 'user_id'
+    t.integer 'learning_unit_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -113,15 +103,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_141732) do
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
-
-  add_foreign_key 'completed_learning_units', 'learning_units'
-  add_foreign_key 'completed_learning_units', 'users'
-  add_foreign_key 'curriculum_affiliations', 'curriculums'
-  add_foreign_key 'curriculum_affiliations', 'learning_units'
-  add_foreign_key 'resource_comments', 'resources'
-  add_foreign_key 'resource_comments', 'users'
-  add_foreign_key 'resource_evaluations', 'resources'
-  add_foreign_key 'resource_evaluations', 'users'
-  add_foreign_key 'resources', 'learning_units'
-  add_foreign_key 'resources', 'users'
 end
--- spec/models/completed_learning_unit_spec.rb
+++ spec/models/completed_learning_unit_spec.rb
@@ -1,7 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe CompletedLearningUnit, type: :model do
-  it 'is valid with valid attributes'
-  it 'is not valid without a user_id'
-  it 'is not valid without a learning_unit_id'
+  context 'when creating an object with the factory' do
+    it do
+      expect(build(:completed_learning_unit)).to be_valid
+    end
+  end
+
+  context 'when has valid attributes' do
+    let(:completed_learning_unit) do
+      build :completed_learning_unit,
+            user: build(:user),
+            learning_unit: build(:learning_unit)
+    end
+
+    it do
+      expect(completed_learning_unit).to be_valid
+    end
+  end
+
+  context 'when has has no user_id' do
+    let(:completed_learning_unit) do
+      build :completed_learning_unit,
+            user: nil
+    end
+
+    it do
+      expect(completed_learning_unit).not_to be_valid
+    end
+  end
+
+  context 'when has has no learning_unit_id' do
+    let(:completed_learning_unit) do
+      build :completed_learning_unit,
+            learning_unit: nil
+    end
+
+    it do
+      expect(completed_learning_unit).not_to be_valid
+    end
+  end
 end
