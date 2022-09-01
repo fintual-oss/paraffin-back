bdd65f35808c0460645cb2e3d80db43395880b38
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-01 10:04:55 -0300

    refactor: removed precision nil from user fields

--- db/schema.rb
+++ db/schema.rb
@@ -109,8 +109,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_195045) do
     t.string 'email'
     t.string 'encrypted_password', default: '', null: false
     t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at', precision: nil
-    t.datetime 'remember_created_at', precision: nil
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
