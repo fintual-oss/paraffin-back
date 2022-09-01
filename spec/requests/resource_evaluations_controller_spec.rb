374ca47087def3d29d275fdf488270b0e57364c9
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-01 10:05:22 -0300

    test: resource evaluation controller

---
+++ spec/requests/resource_evaluations_controller_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe ResourceEvaluationsController, type: :request do
+  describe 'POST /create' do
+    let(:user) { create(:user) }
+    let(:resource) { create(:resource, user:) }
+    let(:params) do
+      { 'resource_id': resource.id, 'resource_evaluations':
+        { 'evaluation': 3 } }
+    end
+
+    def perform
+      post resource_resource_evaluations_path(params)
+    end
+
+    before do
+      sign_in user
+    end
+
+    context 'when trying to create an existing evaluation' do
+      before do
+        create(:resource_evaluation, user:, resource:, evaluation: 4)
+      end
+
+      it 'updates the evaluation value' do
+        perform
+        expect(ResourceEvaluation
+               .find_by(user_id: user.id, resource_id: resource.id)
+               .evaluation).to eq(3)
+      end
+
+      it "doesn't create a new object in the database" do
+        expect { perform }.not_to change(ResourceEvaluation, :count)
+      end
+
+      it 'shows the new evaluation value' do
+        perform
+        follow_redirect!
+        expect(response.body).to include('Your current evaluation is 3')
+      end
+    end
+
+    context 'when trying to create a new evaluation' do
+      it "doesn't display any evaluation for this user-resource before" do
+        get resource_path(resource)
+        expect(response.body).not_to include('Your current evaluation is')
+      end
+
+      it 'creates a new object in the database' do
+        expect { perform }.to change(ResourceEvaluation, :count).by(1)
+      end
+
+      it 'displays the new evaluation' do
+        perform
+        follow_redirect!
+        expect(response.body).to include('Your current evaluation is 3')
+      end
+    end
+  end
+end
