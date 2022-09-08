04e3f82996a3b31f1896d34df204f9eb79892698
Ronald Tichauer <rtichauerv@gmail.com>
2022-09-08 12:07:29 -0300

    test: test the new funcionalities

--- spec/requests/learning_units_controller_spec.rb
+++ spec/requests/learning_units_controller_spec.rb
@@ -4,19 +4,46 @@ RSpec.describe LearningUnitsController, type: :request do
   describe 'GET /show' do
     let(:user) { create(:user) }
     let(:learning_unit) { create(:learning_unit) }
+    let(:curriculum) { create(:curriculum) }
 
     before do
       sign_in user
     end
 
-    context 'when accesing to the learning unit page' do
-      def perform
+    def perform(curriculum_id: nil)
+      if curriculum_id.nil?
         get learning_unit_path(learning_unit)
+      else
+        get learning_unit_path(learning_unit, curriculum_id:)
       end
+    end
 
+    context 'when accesing to the learning unit page' do
       it 'shows the name of the learning unit' do
         perform
         expect(response.body).to include(learning_unit.name)
+      end
+    end
+
+    context 'when accessing from an affiliated curriculum' do
+      it 'lets you go back to the curriculum you got from' do
+        create(:curriculum_affiliation, curriculum:, learning_unit:)
+        perform(curriculum_id: curriculum.id)
+        expect(response.body).to include(curriculum.name)
+      end
+    end
+
+    context 'when not accessing from a curriculum' do
+      it 'does not let you go back to any curriculum' do
+        perform
+        expect(response.body).not_to include('Go back to the ')
+      end
+    end
+
+    context 'when accessing from a curriculum with no affiliation' do
+      it 'does not let you go back to the curriculum' do
+        perform(curriculum_id: curriculum.id)
+        expect(response.body).not_to include('Go back to the ')
       end
     end
   end
