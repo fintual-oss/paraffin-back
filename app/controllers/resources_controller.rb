5d5c43b1ee54b410d42b9337ba8be18bf5259ed5
cecyramallo <cecyramallo@gmail.com>
2022-09-08 18:10:37 -0300

    feat: implement create method to save a new resource in resources controller

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,4 +5,17 @@ class ResourcesController < ApplicationController
     @evaluation = ResourceEvaluation
                   .find_by(user: current_user, resource:)
   end
+
+  def create
+    name = params['resource']['name']
+    learning_unit_id = params['learning_unit_id']
+    url = params['resource']['url']
+    Resource.create!(
+      name:,
+      url:,
+      user: current_user,
+      learning_unit_id:
+    )
+    redirect_to(LearningUnit.find(learning_unit_id))
+  end
 end
