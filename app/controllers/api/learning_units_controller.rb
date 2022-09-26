245e7b8e7af192be635ae1c74a1a6b4e1faae835
alejandroleiva <aleivailabaca@gmail.com>
2022-09-26 12:38:14 -0300

    modify l_u_controller to change instance variables for regular ones

--- app/controllers/api/learning_units_controller.rb
+++ app/controllers/api/learning_units_controller.rb
@@ -1,14 +1,14 @@
 module Api
   class LearningUnitsController < ApplicationController
     def show
-      @learning_unit = LearningUnit.find(params[:id])
-      render json: @learning_unit
+      learning_unit = LearningUnit.find(params[:id])
+      render json: learning_unit
     end
 
     def index
       curriculum = Curriculum.find(params[:curriculum_id])
-      @learning_units = curriculum.learning_units
-      render json: @learning_units
+      learning_units = curriculum.learning_units
+      render json: learning_units
     end
 
     def completed
@@ -16,8 +16,8 @@ module Api
         user: current_user,
         learning_unit_id: params[:learning_unit_id]
       )
-      @completed = completed_by_user ? true : false
-      render json: @completed
+      completed = completed_by_user ? true : false
+      render json: completed
     end
   end
 end
