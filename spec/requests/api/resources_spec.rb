cc337ba899d9e594449f98a94f6512f81995ab15
alejandroleiva <aleivailabaca@gmail.com>
2022-09-29 10:08:23 -0300

    rubocop fixes

--- spec/requests/api/resources_spec.rb
+++ spec/requests/api/resources_spec.rb
@@ -6,42 +6,42 @@ describe 'Resources API' do
   before do |response|
     sign_in user unless response.metadata[:skip_before]
   end
-#   path '/api/learning_units/{learning_unit_id}/resources' do
-#     get 'Returns all Resources from Learning Unit' do
-#       tags 'Resources'
-#       parameter name: :learning_unit_id, in: :path, type: :string
-#       produces 'application/json'
+  #   path '/api/learning_units/{learning_unit_id}/resources' do
+  #     get 'Returns all Resources from Learning Unit' do
+  #       tags 'Resources'
+  #       parameter name: :learning_unit_id, in: :path, type: :string
+  #       produces 'application/json'
 
-#       response '200', 'Success' do
-#         schema type: :array,
-#                items: {
-#                  type: :object,
-#                  properties: {
-#                    id: { type: :integer },
-#                    name: { type: :string },
-#                    url: { type: :string }
-#                  }
-#                }
-#         run_test!
-#       end
-#     end
-#   end
+  #       response '200', 'Success' do
+  #         schema type: :array,
+  #                items: {
+  #                  type: :object,
+  #                  properties: {
+  #                    id: { type: :integer },
+  #                    name: { type: :string },
+  #                    url: { type: :string }
+  #                  }
+  #                }
+  #         run_test!
+  #       end
+  #     end
+  #   end
 
-#   path '/api/resources/{resource_id}/average_evaluation' do
-#     get 'Returns Resource average evaluation ' do
-#       tags 'Resources'
-#       parameter name: :resource_id, in: :path, type: :string
-#       produces 'application/json'
+  #   path '/api/resources/{resource_id}/average_evaluation' do
+  #     get 'Returns Resource average evaluation ' do
+  #       tags 'Resources'
+  #       parameter name: :resource_id, in: :path, type: :string
+  #       produces 'application/json'
 
-#       response '200', 'Success' do
-#         schema type: :object,
-#                properties: {
-#                  average_evaluation: { type: :integer }
-#                }
-#         run_test!
-#       end
-#     end
-#   end
+  #       response '200', 'Success' do
+  #         schema type: :object,
+  #                properties: {
+  #                  average_evaluation: { type: :integer }
+  #                }
+  #         run_test!
+  #       end
+  #     end
+  #   end
 
   path '/api/resources/{id}' do
     get 'Returns a Resource info' do
@@ -54,11 +54,11 @@ describe 'Resources API' do
 
       response '200', 'Success' do
         schema type: :object,
-                properties: {
-                  id: { type: :integer },
-                  name: { type: :string },
-                  url: { type: :string }
-                }
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 url: { type: :string }
+               }
         run_test!
       end
 
