c144d4bcf36823a28940536e7375ecc6d608d7a4
alejandroleiva <aleivailabaca@gmail.com>
2022-09-29 09:55:27 -0300

    create tests and swagger documentations for api/resources_controller, show method

--- spec/requests/api/resources_spec.rb
+++ spec/requests/api/resources_spec.rb
@@ -1,6 +1,11 @@
-# require 'swagger_helper'
+require 'swagger_helper'
 
-# describe 'Resources API' do
+describe 'Resources API' do
+  let(:user) { create(:user) }
+
+  before do |response|
+    sign_in user unless response.metadata[:skip_before]
+  end
 #   path '/api/learning_units/{learning_unit_id}/resources' do
 #     get 'Returns all Resources from Learning Unit' do
 #       tags 'Resources'
@@ -37,4 +42,34 @@
 #       end
 #     end
 #   end
-# end
+
+  path '/api/resources/{id}' do
+    get 'Returns a Resource info' do
+      tags 'Resources'
+      parameter name: :id, in: :path, type: :string
+      produces 'application/json'
+      operationId 'getResource'
+
+      let(:id) { create(:resource, url: 'https://www.google.cl').id }
+
+      response '200', 'Success' do
+        schema type: :object,
+                properties: {
+                  id: { type: :integer },
+                  name: { type: :string },
+                  url: { type: :string }
+                }
+        run_test!
+      end
+
+      response '401', 'Unauthorized', skip_before: true do
+        run_test!
+      end
+
+      response '404', 'Resource not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+    end
+  end
+end
