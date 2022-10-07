f7bce0c895bca8433303c38162fc1f88b03be24b
alejandroleiva <aleivailabaca@gmail.com>
2022-10-06 21:50:30 -0300

    add average method to resource model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -17,4 +17,11 @@ class Resource < ApplicationRecord
   has_many :resource_evaluations
 
   validates :url, url: { allow_blank: false }
+
+  private
+  
+  def average_evaluation
+    resource_evaluations.average(:evaluation)
+  end
+  
 end
