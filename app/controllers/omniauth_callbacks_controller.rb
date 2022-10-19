3b363fe4795dc6f63fc21d69362125979d2dab0e
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-19 11:41:54 -0300

    feat: omniauth callbacks controller

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,6 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def github
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    signin_and_redirect @user
+  end
+end
