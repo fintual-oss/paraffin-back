3f512eb96cd455c2700f036b1d9a729533009fd9
ignacio <iaheck@uc.cl>
2022-10-19 10:59:40 -0300

    feat(cycle): base model and migration run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_123117) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_135844) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_06_123117) do
 
   create_table "curriculums", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "cycles", force: :cascade do |t|
+    t.integer "curriculum_id"
+    t.string "name"
+    t.text "learning_goals"
+    t.text "challenge"
+    t.string "boilerplate_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
