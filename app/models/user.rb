2bd9c4e09cc8130e18392b3d27238779fd2fda83
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-19 11:43:34 -0300

    feat: method from_omniauth in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :resources
   has_many :resource_comments
   has_many :resource_evaluations
   has_many :completed_learning_units
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provideruser.name = auth.info.name
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
