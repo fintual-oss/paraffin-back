75c4fdac3aa7f14206575f2c75450eebf3996104
Pablo Abara <pbabara@uc.cl>
2022-10-19 09:32:37 -0300

    feat(learning_unit): added description and image url to learning units

--- db/schema.rb
+++ db/schema.rb
@@ -85,6 +85,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_142806) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.string "image_url"
   end
 
   create_table "resource_comments", force: :cascade do |t|
