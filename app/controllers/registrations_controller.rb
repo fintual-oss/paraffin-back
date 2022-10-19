1a541098506ab12a3dce5189f039bc32e852f801
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-19 11:40:34 -0300

    feat: modify devise registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,19 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def signup_params
+    params.require(:user).permit( :name,
+                                  :email,
+                                  :password,
+                                  :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit( :name,
+                                  :email,
+                                  :password,
+                                  :password_confirmation,
+                                  :current_password)
+  end
+end
