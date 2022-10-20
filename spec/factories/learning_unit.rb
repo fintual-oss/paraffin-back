180c56245e0c3a612181619c55719edac4f67530
Pablo Abara <pbabara@uc.cl>
2022-10-20 16:16:40 -0300

    test(learning-unit): add description to learning unit factory

--- spec/factories/learning_unit.rb
+++ spec/factories/learning_unit.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :learning_unit do
     name { 'Full Stack Development' }
+    description { 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' }
   end
 end
