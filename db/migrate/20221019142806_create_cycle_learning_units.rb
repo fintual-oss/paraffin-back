acf5e95297717883957886d1b6a94ac73dd010c6
ignacio <iaheck@uc.cl>
2022-10-20 12:02:25 -0300

    feat(cycle): adds not null validations

--- db/migrate/20221019142806_create_cycle_learning_units.rb
+++ db/migrate/20221019142806_create_cycle_learning_units.rb
@@ -1,8 +1,8 @@
 class CreateCycleLearningUnits < ActiveRecord::Migration[7.0]
   def change
     create_table :cycle_learning_units do |t|
-      t.integer :cycle_id
-      t.integer :learning_unit_id
+      t.integer :cycle_id, null: false
+      t.integer :learning_unit_id, null: false
 
       t.timestamps
     end
