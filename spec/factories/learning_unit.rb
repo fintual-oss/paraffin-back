eb5417d29ead9f74c3cab6b05eeb815076fa5d46
Pablo Abara <pbabara@uc.cl>
2022-10-20 16:18:06 -0300

    test(learning-unit): change line that was too long on rubocop application

--- spec/factories/learning_unit.rb
+++ spec/factories/learning_unit.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :learning_unit do
     name { 'Full Stack Development' }
-    description { 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.' }
+    description do
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
+    end
   end
 end
