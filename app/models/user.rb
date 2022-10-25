acadeba53791fd18862ac58c92c8dfc8578a0b2e
isavega <igvega02@gmail.com>
2022-10-25 11:35:22 -0300

    feat: add oauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,20 @@
 #  remember_created_at    :datetime
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :resources
   has_many :resource_comments
   has_many :resource_evaluations
   has_many :completed_learning_units
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+    end
+  end
 end
