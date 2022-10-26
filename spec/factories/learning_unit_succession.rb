7df4475926dd213debb19df31985de892346cbdc
Ronald Tichauer <rtichauerv@gmail.com>
2022-10-26 17:18:36 -0300

    fix(successions-admin-section): successions factory uses learning units that belong to cycle

--- spec/factories/learning_unit_succession.rb
+++ spec/factories/learning_unit_succession.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :learning_unit_succession do
     association :cycle, factory: :cycle
-    predecessor { create(:learning_unit) }
-    successor { create(:learning_unit) }
+    predecessor { create(:learning_unit, cycles: [cycle]) }
+    successor { create(:learning_unit, cycles: [cycle]) }
   end
 end
