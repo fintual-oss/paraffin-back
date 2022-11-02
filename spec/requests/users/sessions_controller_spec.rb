f21706efd435d03b9797e81b0a46000348af28d9
Juan Santos <jjsantosochoa@outlook.com>
2022-11-02 17:34:44 -0300

    refactor(redirect_after_login): replace perform method in sessions_controller_spec

--- spec/requests/users/sessions_controller_spec.rb
+++ spec/requests/users/sessions_controller_spec.rb
@@ -6,27 +6,21 @@ RSpec.describe Users::SessionsController, type: :request do
     let(:curriculum_id) { create(:curriculum).id }
 
     context 'when redirect_to is not present as parameter' do
-      def perform
+      it 'redirects to the root_path' do
         sign_in user
         post user_session_url
-      end
-
-      it 'redirects to the root_path' do
-        expect(perform).to redirect_to(root_path)
+        expect(response).to redirect_to(root_path)
       end
     end
 
     context 'when redirect is present as parameter' do
-      def perform(redirect_path)
-        get new_user_session_path, params: { redirect_to: redirect_path }
-        sign_in user
-        post user_session_url
-      end
-
       let(:redirect_path) { "/curriculums/#{curriculum_id}/learning_units" }
 
       it 'redirects to the provided path' do
-        expect(perform(redirect_path)).to redirect_to(redirect_path)
+        get new_user_session_path, params: { redirect_to: redirect_path }
+        sign_in user
+        post user_session_url
+        expect(response).to redirect_to(redirect_path)
       end
     end
   end
