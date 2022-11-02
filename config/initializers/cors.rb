635f51846a98e0c1474e94c6bad6e6a3a4f190de
isavega <igvega02@gmail.com>
2022-11-02 17:57:30 -0300

    refactor: delete cap-frontend url from cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     if Rails.env.development?
-      origins 'localhost:3000', 'localhost:3001', 'https://cap-frontend-wnyw.vercel.app'
+      origins 'localhost:3000', 'localhost:3001'
     else
       origins 'https://cap-frontend-wnyw.vercel.app'
     end
