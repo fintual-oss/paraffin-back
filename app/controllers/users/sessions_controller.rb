68f82dc9600128e99040a61db2c630daa3672df4
isavega <igvega02@gmail.com>
2022-11-02 17:52:23 -0300

    feat: create omniauth controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,13 +1,16 @@
-module Users
-  class SessionsController < Devise::SessionsController
-    def new
-      self.resource = resource_class.new(sign_in_params)
-      store_location_for(resource, params[:redirect_to])
-      super
-    end
+class Users::SessionsController < Devise::SessionsController
 
-    def after_sign_in_path_for(resource)
-      stored_location_for(resource) || root_path
-    end
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    store_location_for(resource, params[:redirect_to])
+    super
+  end
+  
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
   end
 end
