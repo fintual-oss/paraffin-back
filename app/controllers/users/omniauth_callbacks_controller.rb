dd363b4edd93e2bed5109fea4ab3af20174f8098
isavega <igvega02@gmail.com>
2022-11-03 15:43:55 -0300

    refactor: update google_oauth2 method

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,13 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     user = User.from_omniauth(auth)
-
     if user.present?
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
       flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google',
-                                               reason: "#{auth.info.email}
-                                                is not authorized."
+        t 'devise.omniauth_callbacks.failure', kind: 'Google'
       redirect_to new_user_session_path
     end
   end
