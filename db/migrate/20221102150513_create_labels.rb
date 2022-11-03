d9364df9ea041f75a6c1c1b6dc0b4ad1048f038e
Pablo Abara <pbabara@uc.cl>
2022-11-03 11:32:30 -0300

    fix(label-model): rollback and corrected migration of label model

--- db/migrate/20221102150513_create_labels.rb
+++ db/migrate/20221102150513_create_labels.rb
@@ -2,9 +2,10 @@ class CreateLabels < ActiveRecord::Migration[7.0]
   def change
     create_table :labels do |t|
       t.string :name, null: false
-      t.string :icon, null: false
+      t.string :description, null: false
 
       t.timestamps
+      
     end
   end
 end
--- db/migrate/20221103141540_change_label_icon_to_description.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLabelIconToDescription < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :labels, :icon, :description
-  end
-end
