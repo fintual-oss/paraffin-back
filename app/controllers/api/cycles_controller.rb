50f9a11912ba94e4be155153be3a2e78f7d2c92f
alejandroleiva <aleivailabaca@gmail.com>
2022-11-06 20:56:25 -0300

    feat(complete-cycle): create complete method in api/cycle_controller

--- app/controllers/api/cycles_controller.rb
+++ app/controllers/api/cycles_controller.rb
@@ -1,7 +1,7 @@
 module Api
   class CyclesController < ApiApplicationController
     before_action :set_curriculum, only: [:index]
-    before_action :set_cycle, only: [:show, :mark_as_completed]
+    before_action :set_cycle, only: [:show]
 
     def index
       cycles = @curriculum.cycles
@@ -15,6 +15,25 @@ module Api
                       challenge_description boilerplate_url]
     end
 
+    def complete
+      cycle = Cycle.find(params[:cycle_id])
+      if completed_learning_units?(cycle)
+        UserCycleState.find_or_create_by(user: current_user, cycle:) do |state|
+          state.completed_at = Time.current unless state.completed_at
+        end
+        success
+      else
+        bad_request
+      end
+    end
+
+    def uncomplete
+      cycle = Cycle.find(params[:cycle_id])
+      cycle_state = UserCycleState.find_by(user: current_user, cycle:)
+      cycle_state.completed_at = nil if cycle_state
+      success
+    end
+
     private
 
     def set_curriculum
@@ -25,10 +44,28 @@ module Api
       @cycle = Cycle.find(params[:id])
     end
 
-    def completed_learning_units?
-      @cycle.learning_units.all? do |learning_unit|
+    def completed_learning_units?(cycle)
+      cycle.learning_units.all? do |learning_unit|
         CompletedLearningUnit.exists?(user: current_user, learning_unit:)
       end
+    end
+
+    def create_cycle_state(cycle)
+      UserCycleState.create(cycle:, user: current_user)
+    end
+
+    def success
+      status = :ok
+      code = 200
+      message = 'Success'
+      render json: { status:, code:, message: }, status:
+    end
+
+    def bad_request
+      status = :bad_request
+      code = 400
+      message = 'Bad Request'
+      render json: { status:, code:, message: }, status:
     end
   end
 end
