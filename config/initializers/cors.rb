c733ecb47591a690940cc3c2355d79424d145563
Valentin <valentinjadot@gmail.com>
2022-11-09 15:01:38 -0300

    fix(coors) : add paraffin-front

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins 'localhost:3000', 'localhost:3001'
     else
-      origins 'https://ser.dev'
+      origins 'https://ser.dev', 'https://paraffin-front.vercel.app/'
     end
     resource '*', headers: :any, methods: [:get, :post, :patch]
   end
